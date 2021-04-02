f49c3555492ebe093ba0d57f2695a11a9bbad73c
a11ce <a11ce@protonmail.com>
2021-04-02 04:08:07 -0500

    Additions for katbot integration

--- tarot.py
+++ tarot.py
@@ -1,8 +1,10 @@
 import csv
 
+
 def main():
     pass
-    
+
+
 def loadCards(fileName):
     with open(fileName) as f:
         reader = csv.reader(f, delimiter=",")
@@ -12,9 +14,23 @@ def loadCards(fileName):
             cards.append(dict(zip(cols, line)))
     return cards
 
+
 def asMonoLine(card):
-    formCard = [val + (" " * (colLen-len(val))) for val, colLen in zip(card.values(), [21, 21, 10, 18, 10, 10, 0])]
+    formCard = [
+        val + (" " * (colLen - len(val)))
+        for val, colLen in zip(card.values(), [21, 21, 10, 18, 10, 10, 0])
+    ]
     return "".join(list(formCard))
-    
+
+
+def as60WidthLines(card):
+    data = card.values()
+    s = "```\n{}\t({})\n".format(data[0], data[1])
+    s += "\t{}\n".format(",".join(
+        [aspect for aspect in data[2:6] if aspect != ""]))
+    s += "\t{}\n```".format(data[7])
+    return s
+
+
 if __name__ == "__main__":
     main()
